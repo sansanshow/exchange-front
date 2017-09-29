924170b4e86dcc73675c4445dd39507f88a32f2b
sansan_com <sansan_show@126.com>
2017-09-29 19:20:52 +0800

    components

---
+++ src/components/UiRightWrap.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="content security">
+        <ui-head :options="headOptions" @backEvent="backE"></ui-head>
+        <div class="main">
+             <slot>
+                嗖嗖嗖
+             </slot>
+        </div>
+    </div>
+</template>
+<script>
+import UiHead from './UiHead';
+export default {
+    props:{
+        headOptions:{
+            type: Object,
+            default: () => {
+                return {
+                    title: '标题'
+                };
+            }
+        }
+    },
+    components:{
+        UiHead
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+        backE(){
+            this.$emit('headBack');
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.content{
+    padding-bottom: 134px;
+    .main{
+        padding: 0 34px;
+        
+    }
+}
+</style>
--- src/views/user/userAccount.vue
+++ src/views/user/userAccount.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="">
-    账户管理
-  </div>
+  <ui-right-wrap :headOptions="headOptions">
+aa
+  </ui-right-wrap>
 </template>
 <script>
+import UiRightWrap from '../../components/UiRightWrap'
 export default {
     components:{
-    
+      UiRightWrap
     },
     data(){
         return {
-
+          headOptions:{
+            title: '账户管理',
+            sub: '请不要透露短信和谷歌验证码给任何人，包括我们的客服。',
+            back: '返回'
+          },
         }
     }
 }
